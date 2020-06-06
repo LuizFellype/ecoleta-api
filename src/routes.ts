9693b310d8dfeb4100416060ebc3a5855a8c3e79
Luiz Fellype <lfcassago@gmail.com>
2020-06-06 04:42:02 -0300

    feat: validate data

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import multer from 'multer'
+import { celebrate, Joi } from 'celebrate'
 import multerConfig from './config/multer'
 import knex from './database/connections'
 
@@ -8,16 +9,15 @@ const routes = express.Router()
 routes.get('/items', async (req, res) => {
     try {
         const items = await knex('items').select('*')
-        
+
         const serializedItems = items.map(({ title, image }) => ({ title, image_url: `http://localhost:3333/uploads/${image}` }))
-        
+
         res.json(serializedItems)
     } catch (error) {
         // console.log('<<<<<<<<<<<<<<<------>>>>>>>>>>>>>>>', error)
         res.json(error)
     }
 })
-
 
 routes.get('/points', async (req, res) => {
     const { city, uf, items } = req.query
@@ -50,7 +50,7 @@ routes.get('/point/:id', async (req, res) => {
         const point = await knex('points').where('id', id).first()
 
         if (!point) return res.status(400).json({ message: 'Point not found' })
-        
+
         const serializedPoint = {
             ...point,
             image_url: `http://localhost:3333/uploads/${point.image}`
@@ -67,32 +67,47 @@ routes.get('/point/:id', async (req, res) => {
 })
 
 const upload = multer(multerConfig)
-routes.post('/points', upload.single('image'), async (req, res) => {
-    const {
-        items,
-        ...point
-    } = req.body || {}
-    const _point = { ...point, image: req.file.filename }
+routes.post(
+    '/points',
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            uf: Joi.string().required().max(2),
+            city: Joi.string().required(),
+            items: Joi.string().required(),
+        })
+    }, { abortEarly: false }),
+    async (req, res) => {
+        const {
+            items,
+            ...point
+        } = req.body || {}
+        const _point = { ...point, image: req.file.filename }
 
-    try {
-        const trx = await knex.transaction()
+        try {
+            const trx = await knex.transaction()
 
-        const [point_id] = await trx('points').insert(_point)
+            const [point_id] = await trx('points').insert(_point)
 
-        const pointItems = items.split().map((item_id: string) => ({
-            item_id: Number(item_id.trim()),
-            point_id
-        }))
+            const pointItems = items.split().map((item_id: string) => ({
+                item_id: Number(item_id.trim()),
+                point_id
+            }))
 
-        await trx('point_items').insert(pointItems)
+            await trx('point_items').insert(pointItems)
 
-        await trx.commit()
+            await trx.commit()
 
-        res.json({ ..._point, id: point_id })
-    } catch (err) {
-        res.json({ err: err.message })
-    }
-})
+            res.json({ ..._point, id: point_id })
+        } catch (err) {
+            res.json({ err: err.message })
+        }
+    })
 
 routes.get('/', (req, res) => {
     res.json({ ok: true })
